9fce8070d4683d916629c0589c516ee66ca525f7
Radwan Ahmed Anik || Front-End Engineer || UI || Template Developer | Self Learner <radwancse503@gmail.com>
2023-05-11 01:00:21 +0600

    table handle

--- cypress/e2e/Table.cy.js
+++ cypress/e2e/Table.cy.js
@@ -21,4 +21,52 @@ describe("Handle Table", () => {
       "table[class='table table-bordered table-hover']>thead>tr>td"
     ).should("have.length", "7");
   });
+
+  it.skip("check cell column specific row & col", () => {
+    cy.get(
+      "table[class='table table-bordered table-hover']>tbody>tr:nth-child(5)>td:nth-child(3)"
+    ).contains("princytrainings4@gmail.com");
+  });
+
+  it.skip("Read all the rows & col data in first page", () => {
+    cy.get("table[class='table table-bordered table-hover']>tbody>tr").each(
+      ($row, index, $rows) => {
+        cy.wrap($row).within(() => {
+          cy.get("td").each(($col, index, $cols) => {
+            cy.log($col.text());
+          });
+        });
+      }
+    );
+  });
+
+  it("Pagination", () => {
+    // let totalPages;
+    // cy.get(".col-sm-6.text-end").then((e) => {
+    //   let mytext = e.text();
+    //   totalPages = mytext.substring(
+    //     mytext.indexOf("(") + 1,
+    //     mytext.indexOf("Pages") - 1
+    //   );
+    //   cy.log("Total num of pages" + totalPages);
+    // });
+
+    let totalPages = 5;
+    for (let p = 1; p <= totalPages; p++) {
+      if (totalPages > 1) {
+        cy.log("Active page" + p);
+        cy.get("ul[class='pagination']>li:nth-child(" + p + ")").click();
+        cy.wait(3000);
+        cy.get("table[class='table table-bordered table-hover']>tbody>tr").each(
+          ($row, index, $rows) => {
+            cy.wrap($row).within(() => {
+              cy.get("td:nth-child(3)").then((e) => {
+                cy.log(e.text());
+              });
+            });
+          }
+        );
+      }
+    }
+  });
 });
